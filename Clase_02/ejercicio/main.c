fe2ebcd6567e36f48f8d9526e6a89ee57251e257
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-03-23 12:26:28 -0300

    ejercicio 2

--- Clase_02/ejercicio/main.c
+++ Clase_02/ejercicio/main.c
@@ -1,64 +1,107 @@
-/******************************************************************
-* Programa: Ejemplo Clase 2
-*
-* Objetivo:
-*   -Realizar un programa que solicite cinco números e imprima por pantalla el
-*    promedio, el máximo y el mínimo.
-*
-* Aspectos a destacar:
-*   -El uso de bucles para pedir un número tras otro
-*   -El uso de acumuladores, se tiene que guardar la suma para luego hacer
-*    el promedio
-*   -El uso del if para preguntar si el numero ingresado es mayor o menor
-*    para guardar los máximos y mínimos
-*
-* Version: 0.1 del 29 diciembre de 2015
-* Autor: Mauricio Dávila
-* Revisión: Ernesto Gigliotti
-*
-* *******************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
+/*
+*Se ingresan numeros (!=0) , no se sabe cuantos.
+*Se pide mostrar:
+        *Cantidad de pares e impares.
+        *Porcentaje de numeros positivos y negativos.
+        *Maximo y minimo.
+        *Maximo numero par.
+        *cantidad de numeros comprendidos entre 125 y 236.
 
-
+*/
 int main()
 {
+    int numero;
+    int contadorPares=0;
+    int contador=0;
+    int contadorImpares=0;
+    int contNegativos=0;
+    int contPositivos=0;
+    int cantidadDeNumeros;
+    char rta='s';
+    float porcentajeNegativos;
+    float porcentajePositivos;
     int maximo;
     int minimo;
-    int acumulador;
-    int auxiliarInt;
-    int i;
+    int flag=0;
+    int maximoPar;
+    int numerosEntre=0;
 
-    // Solicito el primer número el cual por ser el primero sera tanto el
-    // maximo como el minimo y el primer numero a ser acumulado
-    printf("Ingrese un numero: ");
-    scanf("%d",&auxiliarInt);
-    maximo = auxiliarInt;
-    minimo = auxiliarInt;
-    acumulador = auxiliarInt;
-
-    // Solicito el resto de los numeros
-    for(i=0;i<4;i++)
+    while(rta!='n')
     {
-        printf("Ingrese un numero: ");
-        scanf("%d",&auxiliarInt);
-        acumulador = acumulador + auxiliarInt;
+        printf("\nIngrese un numero: ");
+        scanf("%d", &numero);
 
-        if(auxiliarInt > maximo)
+        while(numero ==0)
         {
-            maximo = auxiliarInt;
+            printf("\nReingrese un numero: ");
+            scanf("%d", &numero);
+        }
+        if(numero%2==0 ||numero>maximoPar)
+        {
+
+
+        }
+        contadorPares++;
+        else{
+            contadorImpares++;
+        }
+        if(numero>0)
+        {
+            contPositivos++;
+
+        }
+        else{
+            contNegativos++;
+        }
+        /*if (flag==0)
+        {
+            maximo= numero;
+            minimo= numero;
+            flag= 1;
+        }
+        else{
+            if(numero<minimo)
+            {
+                minimo= numero;
+            }
+            if(numero>maximo)
+            {
+                maximo=numero;
+            }
+        }
+*/
+        if(flag==0 || numero>maximo)
+        {
+            maximo=numero;
+
+        }
+        if(flag==0 || numero<minimo)
+        {
+                minimo=numero;
+                flag=1;
         }
 
-        if(auxiliarInt < minimo)
-        {
-            minimo = auxiliarInt;
-        }
+        printf("\nDesea continuar? ");
+        rta= getche();
     }
 
-    printf("El maximo es: %i\n",maximo);
-    printf("El minimo es: %i\n",minimo);
-    printf("El promedio es: %f\n", ((float)acumulador) / 5);
-    return 0;
+    cantidadDeNumeros=contPositivos+contNegativos;
+    if(cantidadDeNumeros>125 || cantidadDeNumeros<236)
+    {
+        numerosEntre++;
+    }
 
+    porcentajePositivos=(float)(contPositivos*100)/cantidadDeNumeros;
+    porcentajeNegativos= 100-porcentajePositivos; //porcentaje de negativo es la diferencia que hay entre los dos//
+
+    printf("\nLa cantidad de pares es: %d", contadorPares);
+    printf("\nLa cantidad de impares es: %d", contadorImpares);
+    printf("\nEl porcentaje de negativos es: %% %f", porcentajeNegativos);
+    printf("\nEl porcentaje de positivos es: %% %f", porcentajePositivos);
+    printf("\nEl maximo es: %d,", maximo);
+    printf("\nEl minimo es: %d,", minimo);
+    printf("\nLos numeros entre 126 y 236: %d",numerosEntre);
+    return 0;
 }
