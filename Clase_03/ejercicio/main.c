a48703af4c6b034fd7ec0d892b74d5644585aea8
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-04-05 18:53:44 -0300

    float

--- Clase_03/ejercicio/main.c
+++ Clase_03/ejercicio/main.c
@@ -1,50 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pedirEntero(char[], int, int);
-
-int validarEntero(int, int, int, char[]);
+float dividirEnteros(int numeroUno,int numeroDos);
 
 int main()
 {
-    int edad;    //18-60
-    int legajo;    //1-1000
-    int nota;       //1-10
-
-    edad = pedirEntero("Ingrese edad: ", 18, 60); //funciona como el prompt y se guarda
-
-    legajo = pedirEntero("Ingrese legajo: ", 1, 1000);
-
-    nota = pedirEntero("Ingrese nota: ",1, 10);
-
-    printf("La edad es: %d, el legajo es %d, y la nota es %d", edad, legajo, nota);
-
-
+    float numero;
+    numero=dividirEnteros();
+    printf("el resultado es %f",numero);
     return 0;
 }
-int pedirEntero(char mensaje[], int min, int max)
+float dividirEnteros(int numeroUno,int numeroDos)
 {
-    int numero;
-
-    printf("%s", mensaje);    //para un conjunto de caracteres y oraciones no pongo %c pongo %s//
-    scanf("%d", &numero);
-
-    numero = validarEntero(numero, min, max, mensaje);
-while(numero<min || numero>max)
-    {
-        printf("Error: %s", mensaje);
-        scanf("%d", &numero);
-    }
-
-    return numero;
-}
-int validarEntero(int dato, int min, int max, char mensaje[])
-{
-    while(dato<min || dato>max)
-    {
-        printf("Error: Reingrese %s", mensaje);
-        scanf("%d", &dato);
-    }
-
-    return dato;
+    float resultado=0;
+    resultado=(float)numeroUno/numeroDos;
+    return resultado;
 }
