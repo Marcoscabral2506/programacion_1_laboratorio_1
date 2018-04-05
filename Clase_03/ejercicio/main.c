b608fdce8c172a69ce16504afdc8565788d1fb38
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-04-05 18:55:08 -0300

    con float

--- Clase_03/ejercicio/main.c
+++ Clase_03/ejercicio/main.c
@@ -1,18 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float dividirEnteros(int numeroUno,int numeroDos);
+float pedirFloat(char[], float, float);
+float validarFloat(float, float, float, char[]);
 
 int main()
 {
-    float numero;
-    numero=dividirEnteros();
-    printf("el resultado es %f",numero);
+    float edad;    //18-60
+    float legajo;    //1-1000
+    float nota;       //1-10
+
+    edad = pedirFloat("Ingrese edad: ", -100, 0); //funciona como el prompt y se guarda
+
+    legajo = pedirFloat("Ingrese legajo: ", -1, -1000);
+
+    nota = pedirFloat("Ingrese nota: ",-1, -10);
+
+    printf("La edad es: %f, el legajo es %f, y la nota es %f", edad, legajo, nota);
+
+
     return 0;
 }
-float dividirEnteros(int numeroUno,int numeroDos)
+float pedirFloat(char mensaje[], float min, float max)
 {
-    float resultado=0;
-    resultado=(float)numeroUno/numeroDos;
-    return resultado;
+    float numero;
+
+    printf("%s", mensaje);    //para un conjunto de caracteres y oraciones no pongo %c pongo %s//
+    scanf("%f", &numero);
+
+    numero = validarFloat(numero, min, max, mensaje);
+
+    return numero;
 }
+
+/*while(numero<min || numero>max)
+    {
+        printf("Error. %s", mensaje);
+        scanf("%d", &numero);
+    }
+
+    return numero;
+}*/
+float validarFloat(float numero, float min, float max, char mensaje[])
+{
+    while(numero<min || numero>max)
+    {
+        printf("Error: %s", mensaje);
+        scanf("%f", &numero);
+    }
+
+    return numero;
+}
+
